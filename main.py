be36bb244c0325648fe1cf857bfd202f64362296
hfs <1161313037@qq.com>
2020-07-27 15:12:10 +0800

    hfs

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 #!/bin/bash
 import os
+import requests
+def send_ding_msg(msg,title,msg_url):
+    url = "https://oapi.dingtalk.com/robot/send?access_token=93fe896e9a02d9343c3b9dc6d5e80eb87374199060051c6398711fc0d8b2a3c9"
+    headers = {
+        "Content-Type": "application/json ;charset=utf-8 "
+    }
+    ding_msg = {
+        "msgtype":"link",
+        "link":{
+            "text":msg,
+            "title":title,
+            "messageUrl":msg_url
+        }
+
+    }
+    requests.post(url=url,json=ding_msg,headers=headers)
 
 if __name__ == "__main__":
-
+    msg = '自动化监控CI/CD提醒'
+    title= "自动化监控CI/CD提醒:请点击查看自动化报告"
     os.system("rm -rf reports")
     #os.system("rmdir/s /q reports")
     os.system("mkdir reports")
@@ -10,4 +27,11 @@ if __name__ == "__main__":
     test_path = os.path.join(dir,"testsuites","login_suit.yml")
     print(test_path)
     os.system(f"hrun % s" % test_path)
-    os.system("copy ")
+    #获取最新的测试报告
+    lastest_report = os.listdir("./reports")[-1]
+    #挂载测试报告到nginx下
+    os.system(f"cp /data/project/python/workspace/hruntest/reports/*  /usr/local/docker/nginx_v/reports")
+    #发送钉钉报告
+    send_ding_msg(msg=msg,title=title,msg_url=f"http://139.9.179.188:8080/{lastest_report}")
+
+
