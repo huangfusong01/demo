1b7d37a67e7428853c58ea4c0d6c648dd62da8c6
hfs <1161313037@qq.com>
2020-07-27 10:57:11 +0800

    hfs

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     dir = os.path.dirname(os.path.abspath("__name__"))
     test_path = os.path.join(dir,"testsuites/login_suit.yml")
     print(test_path)
-    os.system(f"hrun %s" %test_path)
+    os.system(f"hrun %s" % test_path)
