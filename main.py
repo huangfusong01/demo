0f2c92a4d4d21f8843c9c4bc7c645ac0aacfea23
hfs <1161313037@qq.com>
2020-07-27 16:40:57 +0800

    hfs

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ def send_ding_msg(msg,title,msg_url):
     requests.post(url=url,json=ding_msg,headers=headers)
 
 if __name__ == "__main__":
-    msg = '自动化监控CI/CD提醒'
-    title= "自动化监控CI/CD提醒:请点击查看自动化报告"
+    msg = '自动化测试报告CI/CD提醒'
+    title= "自动化测试报告CI/CD提醒:请点击查看自动化报告"
     os.system("rm -rf reports")
     #os.system("rmdir/s /q reports")
     os.system("mkdir reports")
@@ -31,8 +31,8 @@ if __name__ == "__main__":
     #获取最新的测试报告
     lastest_report = os.listdir("./reports")[-1]
     #挂载测试报告到nginx下
-    os.system(f"cp /data/project/python/workspace/hruntest/reports/*  /usr/local/docker/nginx_v/reports")
+    os.system("cp /data/project/python/workspace/hruntest/reports/*  /usr/local/docker/nginx_v/reports")
     #发送钉钉报告
-    send_ding_msg(msg=msg,title=title,msg_url=f"http://139.9.179.188:8080/{lastest_report}")
+    send_ding_msg(msg=msg,title=title,msg_url=f"http://139.9.179.188:8081/{lastest_report}")
 
 
