dbff7b444d3e28498b518c8c55eeea47d8137bb4
hfs <1161313037@qq.com>
2020-07-27 13:34:55 +0800

    hfs

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import os
 
 if __name__ == "__main__":
 
-    #os.system("rm -rf reports")
-    os.system("rmdir/s /q reports")
+    os.system("rm -rf reports")
+    #os.system("rmdir/s /q reports")
     os.system("mkdir reports")
     dir = os.path.dirname(os.path.abspath("__name__"))
     test_path = os.path.join(dir,"testsuites","login_suit.yml")
     print(test_path)
     os.system(f"hrun % s" % test_path)
+    os.system("copy ")
